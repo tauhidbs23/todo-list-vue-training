96d68b6a95bb16a2116c31a84ac8b2e639fd1e89
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2021-09-15 07:53:39 +0600

    task show succesfully done

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,65 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <router-view/>
+    <!-- <router-view/> -->
+    <div class="container">
+      <h1 class="header">Todo List</h1>
+      <div class="todo-container">
+        <div class="input-container">
+          <input
+            type="text"
+            class="input-todo"
+            placeholder="What needs to be done? "
+            v-model="item.text"
+          />
+          <button @click="taskEntry">Add to Task</button>
+        </div>
+        <Tasks :tasks="tasks" />
+        
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Tasks from "./components/Tasks.vue";
+export default {
+  name: "App",
+  components: {
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+      item: {
+        id: Math.floor(Math.random() * 100),
+        text: "",
+        reminder: true,
+      },
+    };
+  },
+  methods: {
+    taskEntry() {
+      this.tasks.push(this.item);
+    },
+  },
+  created() {
+    // this.tasks.push(this.item)
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at school",
+        reminder: true,
+      },
+    ];
+  },
+};
+</script>
 
 <style>
 #app {
@@ -16,5 +72,35 @@
   text-align: center;
   color: #2c3e50;
   font-size: 22px;
+}
+
+.container {
+  min-width: 230px;
+  max-width: 550px;
+  background: #fff;
+  margin: 10px 0 40px 0;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+}
+.header {
+  width: 100%;
+  font-size: 100px;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  text-rendering: optimizeLegibility;
+}
+.container .todo-container {
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 /20%), 0 25px 50px 0 rgb(0 0 0/10%);
+}
+.input-container {
+  padding-right: 40px;
+  border-bottom: 1px solid #d3d3d3;
+}
+.input-todo {
+  width: 100%;
+  padding: 10px 20px;
+  font-size: 18px;
+  border: none;
 }
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div :key="task.id" v-for="task in tasks">
+      <div class="todo-item">
+        <div class="todo-item-left">
+          <input type="checkbox" />
+          <div class="todo-item-label">{{ task.text }}</div>
+        </div>
+        <div class="remove-item">x</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style>
+.todo-item{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 20px;
+    border-bottom: 1px solid #d3d3d3;
+}
+.todo-item-left{
+    display: flex;
+    align-items: center;
+
+}
+input[type="checkbox" i]{
+    background: initial;
+    cursor: default;
+    appearance: auto;
+    box-sizing: border-box;
+    margin: 3px 3px 3px 4px;
+    padding: initial;
+    border: initial;
+}
+.todo-item-label{
+    margin-left: 12px;
+}
+.remove-item{
+    cursor: pointer;
+    color: black;
+}
+</style>
